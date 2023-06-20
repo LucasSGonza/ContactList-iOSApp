6dff0787d8f8f5bb87792b1f5d8a8f392411d70b
Lucas <107816056+LucasSGonza@users.noreply.github.com>
2023-06-20 17:27:00 -0300

    Add files via upload

---
+++ AppsComSwift/testezin/testezin/ViewControllerContactList.swift
@@ -0,0 +1,29 @@
+//
+//  ViewControllerContactList.swift
+//  testezin
+//
+//  Created by Squad Apps on 20/06/23.
+//
+
+import UIKit
+
+class ViewControllerContactList: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
